1caf484691672555522d3aaf57c4ff14b5810c7e
Ted Kassen <ted@streamlabs.com>
2018-11-08 00:17:56 -0800

    P5

---
+++ src/P5_SmallestMultiple.java
@@ -0,0 +1,8 @@
+public class P5_SmallestMultiple {
+    public static void main(String[] args) throws Exception {
+        // Has minimum necessary factors to max all numbers 1 - 20
+        // Determined by hand
+        System.out.println(2 * 2 * 2 * 2 * 3 * 3 * 5 * 7 * 11 * 13 * 17 * 19);
+    }
+
+}
