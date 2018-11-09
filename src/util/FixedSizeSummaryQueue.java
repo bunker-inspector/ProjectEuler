40a46741bd45f625778f4d3d193c11e78e4853cd
Ted Kassen <ted@streamlabs.com>
2018-11-09 10:53:25 -0800

    P7 and PrimeSieve

--- src/util/FixedSizeSummaryQueue.java
+++ src/util/FixedSizeSummaryQueue.java
@@ -8,29 +8,30 @@ import java.util.function.Function;
 public class FixedSizeSummaryQueue<T, A extends BiFunction<T, T, T>, B extends BiFunction<T, T, T>> {
     private BiFunction<T, T, T> in;
     private BiFunction<T, T, T> out;
+    private Function<T, Optional<T>> _push;
     private T summary;
     private LinkedBlockingQueue<T> queue;
     private Long limit;
 
-    private Function<T, Optional<T>> _push = (a -> {
+    private Optional<T> defaultPush(T a)  {
         queue.add(a);
         summary = in.apply(summary, a);
 
         if (queue.size() == limit) {
-           this._push = (b -> {
-               queue.add(b);
-               summary = in.apply(summary, b);
+            this._push = (b -> {
+                queue.add(b);
+                summary = in.apply(summary, b);
 
-               var removed = queue.remove();
+                var removed = queue.remove();
 
-               summary = out.apply(summary, removed);
+                summary = out.apply(summary, removed);
 
-               return Optional.of(removed);
-           });
+                return Optional.of(removed);
+            });
         }
 
         return Optional.empty();
-    });
+    }
 
     public Optional<T> push(T t) {
         return _push.apply(t);
@@ -44,6 +45,15 @@ public class FixedSizeSummaryQueue<T, A extends BiFunction<T, T, T>, B extends B
         queue = new LinkedBlockingQueue<>();
 
         limit = l;
+
+        _push = this::defaultPush;
+    }
+
+    public void flush(T t) {
+        queue.clear();
+        summary = t;
+
+        _push = this::defaultPush;
     }
 
     public T getSummary() {
